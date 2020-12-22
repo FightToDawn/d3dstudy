0bc9b9d9b41baeefa0ae592ce8fdcf347824e808
FIGHTTODAWN <FIGHTTODAWN@DESKTOP-JGCPVUF>
2020-12-22 16:33:37 +0800

    第二次提交

--- d3d/WinMain.cpp
+++ d3d/WinMain.cpp
@@ -1,4 +1,5 @@
-#include <Windows.h>
+#include "Window.h"
+//#include "WindowsMessageMap.h"
 
 //链接 子系统 windows 时 入口函数是WinMain
 int CALLBACK WinMain(
@@ -7,25 +8,20 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow) 
 {
-	const auto pClassName = "d3dwindow";
-	//注册处窗口类
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;			//自己绘制的窗口
-	wc.lpfnWndProc = DefWindowProc;	//默认的窗口过程
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
+	//创建窗口
+	Window wnd(800, 300, "Donkey Fart Box");
 
-	//创建窗口实例
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg,nullptr,0,0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
+	if (gResult == -1){
+		return -1;
+	}
 
-	return 0;
+	return msg.wParam;
 }
---
+++ d3d/Window.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ChiliWin.h"
+#include <optional>
+#include <memory>
+#include <string>
+#include <vector>
+
+class Window
+{
+private:
+	//WindowClass类 单例管理注册/销毁窗口类
+	class WindowClass {
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const WindowClass&) = delete;
+		static constexpr const char* wndClassName = "Chili Direct3D Engine Window";
+		static WindowClass wndClass;	//单例
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const char* name);
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+	//void SetTitle(const std::string& title);
+	//void EnableCursor() noexcept;
+	//void DisableCursor() noexcept;
+	//bool CursorEnabled() const noexcept;
+	//static std::optional<int> ProcessMessages() noexcept;
+private:
+	//void ConfineCursor() noexcept;
+	//void FreeCursor() noexcept;
+	//void ShowCursor() noexcept;
+	//void HideCursor() noexcept;
+	//void EnableImGuiMouse() noexcept;
+	//void DisableImGuiMouse() noexcept;
+	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+public:
+	//Keyboard kbd;
+	//Mouse mouse;
+private:
+	//bool cursorEnabled = true;
+	int width;
+	int height;
+	HWND hWnd;
+	//std::vector<BYTE> rawBuffer;
+	//std::string commandLine;
+};
+
